dc414fd6310dff374b2e0e2017243c27f5516d8a
Petr <petrhaken2@gmail.com>
2023-08-03 18:12:38 +0200

    gamebook bastic functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+  const [question, setQuestion] = useState("")
+  const [options, setOptions] = useState([])
+
+  const [score, setScore] = useState([])
+
+  let storyData = [
+    {
+      id: 1,
+      text: "Před sebou máte řeku v které je dítě a truhla.",
+      question: "Copak zachráníte ?",
+      options: [
+        {
+          text: "Zachráním truhlu",
+          to: 3,
+          curiosity: true,
+        },
+        {
+          text: "Zachráním dítě",
+          to: 2,
+          curiosity: false,
+        }
+      ]
+    },
+    {
+      id: 2,
+      text: "Zachránil jste dítě které Vám dalo lahvičku s lektvarem.",
+      question: "Vypijete lektvar ?",
+      options: [
+        {
+          text: "ANO",
+          to: 4,
+          curiosity: true,
+        },
+        {
+          text: "NE",
+          to: 5,
+          curiosity: false,
+        }
+      ]
+    },
+    {
+      id: 3,
+      text: "Zachránil jste truhlu která je napůl otevřená ale nevíte co je uvnitř.",
+      question: "Otevřete a nahlédnete do truhly ?",
+      options: [
+        {
+          text: "ANO",
+          to: 6,
+          curiosity: true,
+        },
+        {
+          text: "NE",
+          to: 7,
+          curiosity: false,
+        }
+      ]
+    },
+  ]
+
+  function startGame(id = 0){
+    
+    
+    let searchedElement = storyData.find(element => element.id === id)
+    setText(searchedElement.text)
+    setQuestion(searchedElement.question)
+    setOptions(searchedElement.options)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    
+    <div>
+      <button onClick={()=>startGame(1)}>Start game</button>
+      <h2>Gamebook</h2>
+      <p>{text}</p>
+      <p style={{fontWeight: "bold"}}>{question}</p>
+
+      <div className='container'>
+      {options.map((option, index)  => (
+          <button key={index} onClick={()=>startGame(option.to)}>{option.text}</button>
+      ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    </div>
   )
 }
 
